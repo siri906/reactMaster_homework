1122aec52c42ed639919b9c3842f6e74c652089d
siri906 <sirirang906@gmail.com>
2024-08-02 23:42:57 +0900

    challenge 완료

--- app/challenge/page.tsx
+++ app/challenge/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
           <h2>Weather</h2>
         </div>
         <ul className="list_weather">
-          <li>
+          <li className="bg-white">
             <div className="left_area">
               <strong>Casius</strong>
               <p>Mars, 12AM</p>
@@ -20,7 +20,7 @@ export default function Home() {
               <strong>85</strong>
             </div>
           </li>
-          <li>
+          <li className="bg-yellow-400">
             <div className="left_area">
               <strong>Casius</strong>
               <p>Mars, 12AM</p>
@@ -34,7 +34,7 @@ export default function Home() {
               <strong>85</strong>
             </div>
           </li>
-          <li>
+          <li className="bg-teal-400">
             <div className="left_area">
               <strong>Casius</strong>
               <p>Mars, 12AM</p>
@@ -48,7 +48,7 @@ export default function Home() {
               <strong>85</strong>
             </div>
           </li>
-          <li>
+          <li className="bg-orange-600 text-white">
             <div className="left_area">
               <strong>Casius</strong>
               <p>Mars, 12AM</p>
@@ -88,21 +88,21 @@ export default function Home() {
                 </svg>
               </div>
             </li>
-            <li className="bg-yellow-400">
+            <li className="bg-emerald-400">
               <div className="bx_svg">
                 <svg data-slot="icon" fill="none" stroke-width="1" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"></path>
                 </svg>
               </div>
             </li>
-            <li className="bg-yellow-400">
+            <li className="bg-pink-400">
               <div className="bx_svg">
                 <svg data-slot="icon" fill="none" stroke-width="1" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"></path>
                 </svg>
               </div>
             </li>
-            <li className="bg-yellow-400">
+            <li className="bg-orange-600">
               <div className="bx_svg">
                 <svg data-slot="icon" fill="none" stroke-width="1" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"></path>
@@ -115,7 +115,117 @@ export default function Home() {
           <button>Let&apos;s get it done</button>
         </div>
       </div>
-      <div className="third">dgd</div>
+      <div className="third">
+        <div className="t_area">
+          <button className="circle_button">&#60;</button>
+          <h2>Friends</h2>
+        </div>
+        <div className="search_area">
+          <div className="ipt_area">
+            <input type="text" placeholder="Search with love..." />
+            <button>
+              <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"></path>
+                <span className="blind">검색</span>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="user_area">
+          <ul className="list_user">
+            <li>
+              <div className="left_area">
+                <div className="bx_profile_img">
+                  <svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                  </svg>
+                </div>
+                <div className="bx_profile_info">
+                  <h3>Bill Rizer</h3>
+                  <p>planet Designer</p>
+                </div>
+              </div>
+              <div className="right_area">
+                <div className="btn_area">
+                  <button>Invite</button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="left_area">
+                <div className="bx_profile_img">
+                  <svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                  </svg>
+                </div>
+                <div className="bx_profile_info">
+                  <h3>Genbei Yagy</h3>
+                  <p>planet Designer</p>
+                </div>
+              </div>
+              <div className="right_area">
+                <div className="btn_area">
+                  <button>invite</button>
+                </div>
+              </div>
+            </li>
+            <li className="invited">
+              <div className="left_area">
+                <div className="bx_profile_img">
+                  <svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                  </svg>
+                </div>
+                <div className="bx_profile_info">
+                  <h3>Bill Rizer</h3>
+                  <p>planer Designer</p>
+                </div>
+              </div>
+              <div className="right_area">
+                <div className="btn_area">
+                  <button>Invited</button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="left_area">
+                <div className="bx_profile_img">
+                  <svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                  </svg>
+                </div>
+                <div className="bx_profile_info">
+                  <h3>Lancy Neo</h3>
+                  <p>Rogue Corp</p>
+                </div>
+              </div>
+              <div className="right_area">
+                <div className="btn_area">
+                  <button>Invite</button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="left_area">
+                <div className="bx_profile_img">
+                  <svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                  </svg>
+                </div>
+                <div className="bx_profile_info">
+                  <h3>Konami</h3>
+                  <p>Xenon Creator</p>
+                </div>
+              </div>
+              <div className="right_area">
+                <div className="btn_area">
+                  <button>Invite</button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
